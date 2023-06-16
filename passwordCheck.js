f6d55619fadfdfc4fbd8da656ed2cf090616475b
Takuma Tanabe <takuma@qburst.com>
2023-06-16 15:43:14 +0900

    Password Validation Commit

--- passwordCheck.js
+++ passwordCheck.js
@@ -1,14 +1,30 @@
-function checkPassword(inputtxt)
-{
-var paswd= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{1,50}$/;
-if(inputtxt.value.match(paswd))
-{
-alert('Correct')
-return true;
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var number = document.getElementById("number");
+var special = document.getElementById("special");
+
+var lowerCaseLetters = /[a-z]/g;
+if(myInput.value.match(letter)) {  
+  letter.classList.remove("invalid");
+  letter.classList.add("valid");
+} else {
+  letter.classList.remove("valid");
+  letter.classList.add("invalid");
 }
-else 
-{
-alert('Wrong')
-return false;
-}
-}
+  var number = /[0-9]/g;
+  if(myInput.value.match(number)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  var special = /[!@#$%^&*]/g;
+  if(myInput.value.match(special)) {  
+    special.classList.remove("invalid");
+    special.classList.add("valid");
+  } else {
+    special.classList.remove("valid");
+    special.classList.add("invalid");
+  }
